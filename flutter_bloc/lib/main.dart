75502291d532da35496818b50e481b8c400cd173
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2022-06-29 22:46:02 -0500

    :lipstick: Pages created

--- flutter_bloc/lib/main.dart
+++ flutter_bloc/lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/pages/page1.dart';
+import 'package:flutter_bloc/pages/page2.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'pagina1',
+      routes: {
+        'pagina1': (_) => const Pagina1Page(),
+        'pagina2': (_) => const Pagina2Page(),
+      },
     );
   }
 }
